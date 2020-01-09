fe9ef70958b470b2b121ae499bdd1349a9063437
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-09 12:16:19 +0300

    create a project model

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+   title = models.CharField(max_length =30)
+   # image = ImageField(blank=True, manual_crop="")
+   description = models.TextField()
+   live_link = models.CharField(max_length = 50)
+   
+   def __str__(self):
+      return self.title
