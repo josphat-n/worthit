5ae48bf00bb0d4ed8dcb3ef8782541eec4205b87
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-10 09:40:37 +0300

    define a route and template for posing a project

--- rate/urls.py
+++ rate/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
    path('', views.home, name ='worthit-home'),
+   path('project/', views.project, name="new-project"),
 ]  
--- rate/views.py
+++ rate/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .forms  import ProjectForm
 # Create your views here.
 
 def home(request):
    return render(request, 'rate/home.html')
+
+def project(request):
+   form = ProjectForm()
+   
+   return render(request, 'rate/new-project.html', {"prjForm": form})
