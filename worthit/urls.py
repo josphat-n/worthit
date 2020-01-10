0178ca441ed52a302fbff0223d21e545f7d715b1
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-10 12:44:12 +0300

    define settings and templates for registering and login

--- worthit/urls.py
+++ worthit/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rate.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('login/', LoginView.as_view(template_name= '/'), name='user_login'),
+    path('logout/', LogoutView.as_view(template_name='/'), name="user_logout"),
 ]
