1e7d9ab4d3c1650e3bedc2bdb123eac51cd6999f
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-15 02:33:49 +0300

    require a user to be signedin to upload a project

--- rate/views.py
+++ rate/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .forms  import ProjectForm
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+# Create your views here.
 def home(request):
    return render(request, 'rate/home.html')
 
+@login_required(login_url='/accounts/login/')
 def project(request):
    form = ProjectForm()
    
