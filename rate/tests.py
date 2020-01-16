4caa69df4902caeb1a1d5f367706dca15c7e54d4
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-16 09:11:38 +0300

    create test cases for the project model

--- rate/tests.py
+++ rate/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Profile
+from .models import Profile, Project
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -29,3 +29,29 @@ class ProfileTestClass(TestCase):
       self.profile_one.delete_profile()
       prof = Profile.objects.all()
       self.assertTrue(len(prof)<1)            
+      
+class ProjectTestClass(TestCase):
+   # Setup Method
+   def setUp(self):
+      self.profile=Profile(id = 1)
+      self.project_one=Project(title='git-search',description='Consumes the github API and allows serches in github',live_link= 'httpsa://gitthub.com/Josephat-n/gitSearch') 
+      
+   # Testing Instance
+   def test_instance(self):
+      self.assertTrue(isinstance(self.project_one,Project))  
+      
+   # Testing Save Method
+   def test_save_method(self):
+      self.project_one.save_project()
+      projs = Project.objects.all()
+      self.assertTrue(len(projs) > 0)       
+   # Teardown Method
+   def tearDown(self):
+      Project.objects.all().delete()    
+       
+   #Delete Method   
+   def test_delete(self):
+      self.project_one.save_project()    
+      self.project_one.delete_project()
+      projs = Project.objects.all()
+      self.assertTrue(len(projs)<1)      
