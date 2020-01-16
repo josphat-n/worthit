9d76e5c0b711f50e0701beffc0413f1268047966
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-16 08:30:53 +0300

    define the name of the user in the profile the name that they sign up with

--- rate/models.py
+++ rate/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+   name = models.ForeignKey(User,on_delete=models.CASCADE)
    picture = ImageField(blank=True, manual_crop="")
    bio = models.TextField()   
    contact = models.CharField(max_length =30)
