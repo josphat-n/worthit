a2e98c374306591cab84ee99b87474c82eaeeed0
Josephat-n <josphatnjoroge254@gmail.com>
2020-05-12 18:09:57 +0300

    make relevant changes for authentication upgrade from django-registration 2.5.1 to 3.1

--- worthit/urls.py
+++ worthit/urls.py
@@ -5,7 +5,8 @@ from django.contrib.auth.views import LogoutView, LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rate.urls')),
-    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
     path('login/', LoginView.as_view(template_name= '/'), name='user_login'),
     path('logout/', LogoutView.as_view(template_name='/'), name="user_logout"),
 ]
